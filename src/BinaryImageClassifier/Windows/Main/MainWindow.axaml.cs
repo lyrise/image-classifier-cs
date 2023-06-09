c65c36bd8088f5007520de3b9ea670829f41efba
Lyrise <lyrise1984@gmail.com>
2023-06-10 04:44:45 +0900

    fix

--- src/BinaryImageClassifier/Windows/Main/MainWindow.axaml.cs
+++ src/BinaryImageClassifier/Windows/Main/MainWindow.axaml.cs
@@ -28,19 +28,19 @@ public partial class MainWindow : Window
     {
         if (this.DataContext is IMainWindowModel windowModel)
         {
-            if (e.Key == Key.Up)
+            if (e.Key == Key.Up || e.Key == Key.K)
             {
                 windowModel.UndoCommand.Execute();
             }
-            else if (e.Key == Key.Right)
+            else if (e.Key == Key.Right || e.Key == Key.L)
             {
                 windowModel.RightCommand.Execute();
             }
-            else if (e.Key == Key.Left)
+            else if (e.Key == Key.Left || e.Key == Key.H)
             {
                 windowModel.LeftCommand.Execute();
             }
-            else if (e.Key == Key.Down)
+            else if (e.Key == Key.Down || e.Key == Key.J)
             {
                 windowModel.DownCommand.Execute();
             }
